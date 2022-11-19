cfb35d512e1d4970394c97e30505a5a268ff1ff0
zigameni <shehu.meni@gmail.com>
2022-11-19 11:46:48 +0100

    Formated

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ plt.legend(['K1', 'K2'])
 plt.show()
 """
 
-# Kreiranje ulaza i zeljenog izlaza Perceptrona 
+# Kreiranje ulaza i zeljenog izlaza Perceptrona
 # Bias ce biti predstavljen kao ulaz koji je uvek jednak jedinici
 X = np.concatenate((K1, K2), axis=0)
 bias = np.ones((2*N, 1))
@@ -37,35 +37,40 @@ bias = np.ones((2*N, 1))
 X = np.concatenate((X, bias), axis=1)
 D = np.concatenate((D1, D2), axis=0)
 
-# Kreiranje i trening perceptrona 
-# u pitanju je problem klasifikacije oblika u dve klase, potrebno je 
-# kreirati LTU perceptron koji se obucava perceptronskim pravilom obucavanja 
+# Kreiranje i trening perceptrona
+# u pitanju je problem klasifikacije oblika u dve klase, potrebno je
+# kreirati LTU perceptron koji se obucava perceptronskim pravilom obucavanja
 
-# Definisanje funkcije za racunanje izlaza perceptrona 
+# Definisanje funkcije za racunanje izlaza perceptrona
+
+
 def predict(x, w):
     act = np.sum(w*x)
     if act >= 0:
         y = 1
     else:
         y = 0
-    
+
     return y
 
-# definisanje funkcije za racunanje promene pojacanja 
+# definisanje funkcije za racunanje promene pojacanja
+
+
 def weight_update(lr, d, y, x):
     return lr * (d-y) * x
 
-# Definisanje parametara potrebnih za obucavanje 
-W = 0.1*rnd.randn(3) # pocetne tezine 
-n = 10      # maksimalan broj epoha za obucavanje 
-lr = 0.01   # konstanta za obucavanje 
-Emax = 0.02 # Maksimalna dovoljena greska 
 
-# Obucavanje perceptrona: potrebno je u svakoj epohi proci kroz sve primere ulaznog skupa 
-for epoch in range(n):      # sve epohe treniranja 
+# Definisanje parametara potrebnih za obucavanje
+W = 0.1*rnd.randn(3)  # pocetne tezine
+n = 10      # maksimalan broj epoha za obucavanje
+lr = 0.01   # konstanta za obucavanje
+Emax = 0.02  # Maksimalna dovoljena greska
+
+# Obucavanje perceptrona: potrebno je u svakoj epohi proci kroz sve primere ulaznog skupa
+for epoch in range(n):      # sve epohe treniranja
     error = 0
-    
-    for k in range(len(D)): # Svaki element ulaznog skupa 
+
+    for k in range(len(D)):  # Svaki element ulaznog skupa
         Ypred = predict(W, X[k, :])
         dW = weight_update(lr, D[k], Ypred, X[k, :])
         W += dW
@@ -75,12 +80,12 @@ for epoch in range(n):      # sve epohe treniranja
     error /= np.shape(X)[0]
     print(">epoch=%d, lrate=%.3f, error=%.3f" % (epoch+1, lr, error))
 
-    #if error<Emax: 
+    # if error<Emax:
     #    break
 
-# Prikaz granice odlucivanja 
-# Granica odlucivanja je prava h(x), koja razdvaja prostor obelezja na dva podprostora 
-# od kojih svaki odgovara jendoj klasi. Kako je perceptron linearni klasifikator. 
+# Prikaz granice odlucivanja
+# Granica odlucivanja je prava h(x), koja razdvaja prostor obelezja na dva podprostora
+# od kojih svaki odgovara jendoj klasi. Kako je perceptron linearni klasifikator.
 # h(x) = w1*x1 + w2*x2 + wb = 0
 # iz prednoh izraza imamo da je: x2 = (-w1/w2)*x1 - wb/w2
 
@@ -89,24 +94,9 @@ w2 = W[1]
 wb = W[2]
 
 x1 = np.linspace(-2, 6)
-"""
-x2 = -w1*x1/w2 - wb/w2
-# Prikaz granice odlučivanja
-plt.figure()
-plt.plot(x11, x21, 'o')
-plt.plot(x12, x22, '*')
-plt.plot(x1, x2)
-plt.xlabel('x1')
-plt.ylabel('x2')
-plt.ylim([-2, 6])
-plt.grid()
-plt.legend(['K1', 'K2', 'h(x)=0'])
-plt.show()
-
-"""
 x2 = -w1*x1/w2 - wb/w2
 
-# prikaz granice odlucivanja 
+# prikaz granice odlucivanja
 plt.figure()
 plt.plot(x11, x21, 'o')
 plt.plot(x12, x22, '*')
